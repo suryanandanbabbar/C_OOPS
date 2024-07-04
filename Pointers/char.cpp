f711af1d59350d0d6b15b176f441db71427d8c34
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2024-07-04 18:07:40 +0530

    feat: pointers to char

--- Pointers/char.cpp
+++ Pointers/char.cpp
@@ -2,4 +2,23 @@
 
 int main()
 {
+    // Declaring pointer
+    char *ptr = nullptr;
+
+    // Declaring Variable
+    char variable = 'A';
+
+    // Assigning Variable
+    ptr = &variable;
+
+    std::cout << "The value stored in pointer is: " << *ptr << std::endl; // A
+
+    // String Literal
+    char *ptr1 = {"Hello World!"};
+
+    // Without Asterisk
+    std::cout << "The string literal stored is: " << ptr1 << std::endl; // Hello World!
+
+    // With Asterisk
+    std::cout << "The string literal stored is: " << *ptr1 << std::endl; // H
 }
